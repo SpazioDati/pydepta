c671028567537ddbbb4f6b52b4a631c838bb851f
Davide Setti <setti@spaziodati.eu>
2025-11-19 13:23:34 +0100

    Add pyproject.toml and modern build system
    - Add pyproject.toml with Python 3.10+ requirement
    - Convert README.rst to README.md
    - Simplify setup.py, remove conditional Cython logic
    - Update GitHub Actions to use python -m build
    - Fix lxml include paths for Cython compilation
    - Update .gitignore for dist and .venv

--- setup.py
+++ setup.py
@@ -1,41 +1,23 @@
 import sys
 from setuptools import setup, find_packages
 from distutils.extension import Extension
-import site
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
+from Cython.Distutils import build_ext
+import lxml
 
-cmdclass = {}
-ext_modules = []
+LXML_PATH = lxml.__path__[0]
+INCLUDE_DIRS = [LXML_PATH, LXML_PATH + '/includes']
 
-BASEDIR = [x for x in sys.path if 'site-package' in x][0]
-INCLUDE_DIRS = [BASEDIR + '/lxml', BASEDIR + '/lxml/includes']
-if use_cython:
-    ext_modules.append(Extension(
+ext_modules = [
+    Extension(
         "pydepta.trees_cython",
         ['pydepta/trees_cython.pyx'],
         include_dirs=INCLUDE_DIRS
-    ))
-    cmdclass.update({'build_ext': build_ext})
-else:
-    ext_modules.append(Extension(
-        "pydepta.trees_cython",
-        ['pydepta/trees_cython.c'],
-        include_dirs=INCLUDE_DIRS
-    ))
+    )
+]
 
-setup(name='pydepta',
-      version='0.2',
-      description="A Python implementation of DEPTA",
-      long_description=("A Python implementation of DEPTA (Data Extraction "
-                        "with Partial Tree Alignment)"),
-      author="Terry Peng",
-      author_email="pengtaoo@gmail.com",
-      install_requires=['w3lib', 'sd-scrapely'],
+cmdclass = {'build_ext': build_ext}
+
+setup(
       packages=find_packages(),
       cmdclass=cmdclass,
       ext_modules=ext_modules
